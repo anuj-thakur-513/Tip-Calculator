29b749a998bc51fcf1f2b5781a89cdfd510c44a4
Anuj Thakur <anujthakur2003.anuj@gmail.com>
2022-09-16 18:41:17 +0530

    added the top header

---
+++ app/src/main/java/com/project/jettipcalculator/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.project.jettipcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.project.jettipcalculator.ui.theme.JetTipCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                TopHeader()
+            }
+        }   
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    JetTipCalculatorTheme() {
+        // A surface container
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "₹$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetTipCalculatorTheme {
+        MyApp {
+            Text(text = "Hello Again")
+        }
+    }
+}
